0ae319774ed6e88ab4758f1fd8175c021cadfbca
konstantin.emyashev <kioflex12>
2022-07-01 22:03:48 +0300

    Базовый класс для сценовых компонентов

---
+++ Assets/Scripts/Behavior/Gameplay/GameComponent.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using Utils;
+
+namespace Behavior.Gameplay
+{
+    public abstract class GameComponent : MonoBehaviour
+    {
+        protected virtual void CheckDescription(){ }
+
+        protected virtual void Awake()
+        {
+            if (!Application.isPlaying)
+            {
+                return;
+            }
+            GameComponentUtils.CheckAttributes(this);
+            CheckDescription();
+        }
+
+        protected void OnValidate()
+        {
+            if ( (gameObject.hideFlags & HideFlags.DontSaveInEditor) != 0 ) {
+                return;
+            }
+            GameComponentUtils.CheckAttributes(this);
+            CheckDescription();
+        }
+    }
+}
+
